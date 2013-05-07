904e4628c92e0d97407c4afc336bc51a51eeffd8
Willy Fernandez <willyfernandez12@gmail.com>
2013-05-07 16:46:07 -0500

    borrado de bd

--- BD/trigger.sql
+++
@@ -1,7 +0,0 @@
- DELIMITER |
- CREATE TRIGGER update_total_cantidad AFTER INSERT ON ejemplares
-	FOR EACH ROW
-		BEGIN  
-			update textos set cantidad_total = cantidad_total + 1 where textos.id = new.texto_id;
-		END
-|
